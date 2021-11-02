5acd2385065e010e9a646a5ae51d0d65b63d190c
Dairo01001 <garcianaranjodairo@gmail.com>
2021-11-02 00:26:36 -0500

    Bod Conduit

---
+++ URI_1589.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+
+var lines = input.split("\n");
+
+let t = +lines.shift();
+
+for (let i = 0; i < t; i++) {
+  let arr = lines[i].split(" ").map((number) => +number);
+  console.log(arr.reduce((suma, number) => suma + number, 0));
+}
